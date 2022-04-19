2ad0546470411b1932a018af24b240706d9897ee
Tran Khanh <63302419+khalongtvh@users.noreply.github.com>
2022-04-19 19:54:28 +0700

    s

---
+++ Bai1/Bai_1/Bai_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bai_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nhập vào diện tích S của một mặt cầu.Tính thể tích V của hình cầu này
+            Console.Write("Nhap dien tich : ");
+            Double S = Double.Parse(Console.ReadLine());
+            Double pi = 3.141593;
+            Double R = Math.Sqrt(S / (4 * pi));
+            Double V = (S * R) / 3;
+            Console.WriteLine("The tich : " +  V);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Bai22/Bai22/Bai22/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bai22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Viết chương trình liệt kê, đếm và tính tổng các ước số của số nguyên dương n
+            Console.Write("Nhap so nguyen duong n : ");
+            int n = int.Parse(Console.ReadLine());
+            int count = 0, sum = 0 ;
+            Console.Write("Uoc : ");
+            for (int i = 1; i<=n; i++)
+            {
+                if(n % i == 0)
+                {
+                    Console.Write(i+" ");
+                    count++;
+                    sum += i;
+                }
+            }
+            Console.WriteLine("\nSo uoc : {0} \nTong uoc : {1}",count, sum);
+        }
+    }
+}
