b03277fc73d154302242ed81686e4c109b6f6de5
Tran Khanh <63302419+khalongtvh@users.noreply.github.com>
2022-04-19 20:41:47 +0700

    bai1

--- ConsoleApplication1/ConsoleApplication1/Officer.cs
+++ ConsoleApplication1/ConsoleApplication1/Officer.cs
@@ -74,6 +74,5 @@ namespace ConsoleApplication1
             this.Gender = gender;
             this.Address = address;
         }
-        
     }
 }
--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,9 +8,56 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            ManagerOfficer manager = new ManagerOfficer();
+            while (true)
+            {
+                Console.WriteLine("Application Manager Officer");
+                Console.WriteLine("Enter 1: To insert officer.");
+                Console.WriteLine("Enter 2: To search officer by name.");
+                Console.WriteLine("Enter 3: To show information officers.");
+                Console.WriteLine("Enter 4: To exit.");
+                Console.Write("Enter your choose : ");
 
+
+                string choose = Console.ReadLine();
+                switch (choose)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("Enter a: to insert Enginner");
+                        Console.WriteLine("Enter b: to insert Worker");
+                        Console.WriteLine("Enter c: to insert Staff");
+                        Console.Write("Enter your choose : ");
+
+                        string chooseInsert = Console.ReadLine();
+                        switch (chooseInsert)
+                        {
+                            case "a":
+                                Console.Write("Enter name: ");
+                                string name = Console.ReadLine();
+
+                                Console.Write("Enter age: ");
+                                int age = int.Parse(Console.ReadLine());
+
+                                Console.Write("Enter gender: ");
+                                string gender = Console.ReadLine();
+
+                                Console.Write("Enter address: ");
+                                string address = Console.ReadLine();
+
+                                Console.Write("Enter branch: ");
+                                string branch = Console.ReadLine();
+
+                                manager.addOfficer(new Engineer(name, 15, gender, address, branch));
+                                manager.showListInforOfficer();
+                                break;
+                        }
+                        break;
+                }
+            }
+            
         }
     }
 }
