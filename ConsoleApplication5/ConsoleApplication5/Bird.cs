8b0ab00083c654c78411977632b3a541cdb7e254
Tran Khanh <63302419+khalongtvh@users.noreply.github.com>
2022-04-26 08:09:53 +0700

    abstract

---
+++ ConsoleApplication5/ConsoleApplication5/Bird.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication5
+{
+    public class Bird : Animal
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("Con chim kêu quạt quạt");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Con chim ăn hạt");
+        }
+
+        public void fly()
+        {
+            Console.WriteLine("Con chim biết bay");
+        }
+    }
+}
---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.AnimalSound();
+            cat.Eat();
+            cat.run();
+
+            Bird b = new Bird();
+            b.AnimalSound();
+            b.Eat();
+            b.fly();
+        }
+    }
+}
