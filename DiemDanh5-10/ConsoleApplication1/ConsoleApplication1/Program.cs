6433c31bfab1877563d90f4ddfde7a6e5219a33a
Tran Khanh <63302419+khalongtvh@users.noreply.github.com>
2022-05-10 07:42:41 +0700

    Update Program.cs

--- DiemDanh5-10/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ DiemDanh5-10/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,34 +10,39 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[10];
-            arr = nhapMang(arr);
-            xuatMang(arr);
-            Console.WriteLine($"max : {max_Array(arr)}");
-            Console.WriteLine($"min : {min_Array(arr)}");
+            int[] arr_229 = new int[10];
+            arr_229 = nhapMang(arr_229);
+            Console.WriteLine("mang da nhap: ");
+            xuatMang(arr_229);
+            Console.WriteLine($"max : {max_Array(arr_229)}");
+            Console.WriteLine($"min : {min_Array(arr_229)}");
+            Console.WriteLine("mang sap xep giam dan: ");
+            sapxepGiamDan(arr_229);
+            Console.WriteLine("mang sap xep tang dan: ");
+            sapxepTangDan(arr_229);
             Console.ReadKey();
         }
 
-        static int max_Array(int [] arr)
+        static int max_Array(int [] arr_229)
         {
-            int max = arr[0];
-            for(int i = 1; i< arr.Length; i++)
+            int max = arr_229[0];
+            for(int i = 1; i< arr_229.Length; i++)
             {
-                if(max < arr[i])
+                if(max < arr_229[i])
                 {
-                    max = arr[i];
+                    max = arr_229[i];
                 }
             }
             return max;
         }
-        static int min_Array(int[] arr)
+        static int min_Array(int[] arr_229)
         {
-            int min = arr[0];
-            for (int i = 1; i < arr.Length; i++)
+            int min = arr_229[0];
+            for (int i = 1; i < arr_229.Length; i++)
             {
-                if (min > arr[i])
+                if (min > arr_229[i])
                 {
-                    min = arr[i];
+                    min = arr_229[i];
                 }
             }
             return min;
@@ -51,31 +56,50 @@ namespace ConsoleApplication1
             b = temp;
         }
 
-        static void sapxep(int []arr)
+        static void sapxepGiamDan(int[] arr_229)
         {
-            for(int i = 0; i< arr.Length-1; i++)
+            for (int i = 0; i < arr_229.Length - 1; i++)
             {
-                for(int j = i+1; j<arr.Length; j++)
+                for (int j = i + 1; j < arr_229.Length; j++)
                 {
-                    if(arr[i]<arr[j])
+                    if (arr_229[i] < arr_229[j])
+                    {
+                        swap(ref arr_229[i], ref arr_229[j]);
+                    }
                 }
             }
+
+            xuatMang(arr_229);
         }
-        static int[] nhapMang(int []arr)
+        static void sapxepTangDan(int[] arr_229)
         {
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < arr_229.Length - 1; i++)
             {
-                Console.Write($"arr[{i}] = ");
-                arr[i] = int.Parse(Console.ReadLine());
+                for (int j = i + 1; j < arr_229.Length; j++)
+                {
+                    if (arr_229[i] > arr_229[j])
+                    {
+                        swap(ref arr_229[i], ref arr_229[j]);
+                    }
+                }
             }
-            return arr;
+
+            xuatMang(arr_229);
+        }
+        static int[] nhapMang(int []arr_229)
+        {
+            for (int i = 0; i < arr_229.Length; i++)
+            {
+                Console.Write($"arr_229[{i}] = ");
+                arr_229[i] = int.Parse(Console.ReadLine());
+            }
+            return arr_229;
         }
 
-        static void xuatMang (int[] arr){
-            Console.WriteLine("Mang da nhap: ");
-            for (int i = 0; i < arr.Length; i++)
+        static void xuatMang (int[] arr_229){
+            for (int i = 0; i < arr_229.Length; i++)
             {
-                Console.WriteLine($"arr[{i}] = {arr[i]}");
+                Console.WriteLine($"arr_229[{i}] = {arr_229[i]}");
             }
         }
     }
